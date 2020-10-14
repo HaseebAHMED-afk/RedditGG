e4e092bfb553af2cb48e2be87999ee5b7bb18e51
HaseebAHMED-afk <63052623+HaseebAHMED-afk@users.noreply.github.com>
2020-10-14 21:57:17 +0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import FirstDiv from './Components/FirstDiv';
+import SecondDiv from './Components/SecondDiv';
+import TopNav from './Components/TopNav'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+    <TopNav />
+    <FirstDiv />
+    <SecondDiv />
     </div>
   );
 }
