32c313e35180d3776ed32a7b51c086422a41f073
HaseebAHMED-afk <63052623+HaseebAHMED-afk@users.noreply.github.com>
2020-10-14 22:00:40 +0500

    Created TopNav

---
+++ src/Components/TopNav.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../App.css";
+
+const TopNav = () => {
+  return (
+    <div className="navbar">
+        <img src="./img/redditgg.png" alt="Reddit_Logo" className="reddit_logo"  />
+        <h2 className="reddit-heading">RedditGG</h2>
+    </div>
+  );
+};
+
+export default TopNav;
