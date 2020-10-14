0723e0564574c4e798eef0959ecebbf0aec3a5c0
HaseebAHMED-afk <63052623+HaseebAHMED-afk@users.noreply.github.com>
2020-10-14 22:02:25 +0500

    Created SecondDiv

---
+++ src/Components/SecondDiv.jsx
@@ -0,0 +1,94 @@
+import React from "react";
+
+const SecondDiv = () => {
+  return (
+    <div className="seconddiv">
+      <h1>Take A Look</h1>
+      <div className="row row-cols-1 row-cols-md-2 cardDeck">
+        <div className="card cat-card" style={{ width: "18rem" }}>
+          <img
+            src="https://cdn.wallpapersafari.com/76/68/k6fE7c.jpg"
+            className="card-img-top"
+            alt="csgo"
+          />
+          <div className="card-body">
+            <h5>Counter Strike: Global Offensive</h5>
+            <a>r/csgo</a>
+          </div>
+        </div>
+        <div className="card cat-card" style={{ width: "18rem" }}>
+          <img
+            src="https://cdn.wallpapersafari.com/76/68/k6fE7c.jpg"
+            className="card-img-top"
+            alt="csgo"
+          />
+          <div className="card-body">
+            <h5>Counter Strike: Global Offensive</h5>
+          </div>
+        </div>
+        <div className="card cat-card" style={{ width: "18rem" }}>
+          <img
+            src="https://cdn.wallpapersafari.com/76/68/k6fE7c.jpg"
+            className="card-img-top"
+            alt="csgo"
+          />
+          <div className="card-body">
+            <h5>Counter Strike: Global Offensive</h5>
+          </div>
+        </div>
+        <div className="card cat-card" style={{ width: "18rem" }}>
+          <img
+            src="https://cdn.wallpapersafari.com/76/68/k6fE7c.jpg"
+            className="card-img-top"
+            alt="csgo"
+          />
+          <div className="card-body">
+            <h5>Counter Strike: Global Offensive</h5>
+          </div>
+        </div>
+        <div className="card cat-card" style={{ width: "18rem" }}>
+          <img
+            src="https://cdn.wallpapersafari.com/76/68/k6fE7c.jpg"
+            className="card-img-top"
+            alt="csgo"
+          />
+          <div className="card-body">
+            <h5>Counter Strike: Global Offensive</h5>
+          </div>
+        </div>
+        <div className="card cat-card" style={{ width: "18rem" }}>
+          <img
+            src="https://cdn.wallpapersafari.com/76/68/k6fE7c.jpg"
+            className="card-img-top"
+            alt="csgo"
+          />
+          <div className="card-body">
+            <h5>Counter Strike: Global Offensive</h5>
+          </div>
+        </div>
+        <div className="card cat-card" style={{ width: "18rem" }}>
+          <img
+            src="https://cdn.wallpapersafari.com/76/68/k6fE7c.jpg"
+            className="card-img-top"
+            alt="csgo"
+          />
+          <div className="card-body">
+            <h5>Counter Strike: Global Offensive</h5>
+          </div>
+        </div>
+        <div className="card cat-card" style={{ width: "18rem" }}>
+          <img
+            src="https://cdn.wallpapersafari.com/76/68/k6fE7c.jpg"
+            className="card-img-top"
+            alt="csgo"
+          />
+          <div className="card-body">
+            <h5>Counter Strike: Global Offensive</h5>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SecondDiv;
