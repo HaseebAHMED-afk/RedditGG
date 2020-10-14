1d9d14fac73fb31f312bab08e4152dcbdca089f0
HaseebAHMED-afk <63052623+HaseebAHMED-afk@users.noreply.github.com>
2020-10-14 22:01:27 +0500

    Created FirstDiv

---
+++ src/Components/FirstDiv.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../App.css";
+
+const FirstDiv = () => {
+  return (
+    <div className="firstdiv">
+      <img src="" alt="" />
+      <h1 className="desc">Your place to discuss all things gaming</h1>
+    </div>
+  );
+};
+
+export default FirstDiv;
