cb31a1b6682dcacedae325159055ba9e71975d51
unknown <hammad_205@hotmail.com>
2020-10-16 22:57:08 +0500

    fixed routes and dynamic routes

--- src/App.js
+++ src/App.js
@@ -4,16 +4,23 @@ import FirstDiv from './Components/FirstDiv';
 import SecondDiv from './Components/SecondDiv';
 import TopNav from './Components/TopNav'
 import redditFetch from './Components/redditFetch';
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import PostPage from './Components/PostPage';
 function App() {
 
   const data = redditFetch();
   console.log(data);
   return (
     <div >
+    <Router>
     <TopNav />
-    <FirstDiv />
-    <SecondDiv />
+      <Routes>
+        <Route path='/' element={<FirstDiv />}>
+          <Route path='/' element={<SecondDiv />} />
+        </Route>
+        <Route path=":id" element={<PostPage />} />
+      </Routes>
+    </Router>
     </div>
 
   );
--- src/Components/FirstDiv.jsx
+++ src/Components/FirstDiv.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Outlet } from "react-router";
 import "../App.css";
 
 const FirstDiv = () => {
@@ -6,6 +7,7 @@ const FirstDiv = () => {
     <div className="firstdiv">
       <img src="" alt="" />
       <h1 className="desc">Your place to discuss all things gaming</h1>
+      <Outlet /> {/* needs to be here to render the nested(child) routes */}
     </div>
   );
 };
